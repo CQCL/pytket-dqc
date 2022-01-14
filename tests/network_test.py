90df72df3d8d249edb9ff9303b663be585056fb0
Daniel Mills <danielmills@Daniels-MacBook-Pro.local>
2022-01-14 16:22:26 +0000

    Add placement test

--- tests/network_test.py
+++ tests/network_test.py
@@ -1,6 +1,4 @@
 from pytket_dqc.networks import NISQNetwork, ServerNetwork
-from pytket import Circuit
-from pytket_dqc import DistributedCircuit
 from pytket.routing import Architecture, NoiseAwarePlacement  # type:ignore
 from pytket.circuit import Node  # type:ignore
 import pytest
@@ -79,36 +77,3 @@ def test_server_network_is_placement():
     assert not large_network.is_placement(placement_five)
     assert small_network.is_placement(placement_one)
     assert not small_network.is_placement(placement_two)
-
-
-def test_nisq_network_is_placement():
-
-    large_network = NISQNetwork([[0, 1], [0, 2], [1, 2]], {
-                                0: [0, 1, 2], 1: [3, 4, 5], 2: [6, 7, 8, 9]})
-    small_network = NISQNetwork([[0, 1]], {0: [0, 1], 1: [2]})
-
-    small_circ = Circuit(2).CZ(0, 1)
-    dist_small_circ = DistributedCircuit(small_circ)
-
-    med_circ = Circuit(4).CZ(0, 1).CZ(1, 2).CZ(2, 3)
-    dist_med_circ = DistributedCircuit(med_circ)
-
-    placement_one = Placement({0: 1, 1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1})
-    placement_two = Placement({0: 1, 1: 1, 2: 1, 3: 0, 4: 0, 5: 0, 6: 2})
-    placement_three = Placement({0: 2, 1: 2, 2: 2, 3: 2, 4: 0, 5: 0, 6: 0})
-    placement_four = Placement({0: 0, 1: 0, 2: 0, 3: 2, 4: 0, 5: 0, 6: 0})
-    placement_five = Placement({0: 0, 1: 0, 2: 0, 3: 0})
-    placement_six = Placement({0: 2, 1: 2, 2: 3})
-    placement_seven = Placement({0: 1, 1: 1, 2: 1, 3: 0, 4: 1, 5: 1, 6: 1})
-    placement_eight = Placement({0: 1, 1: 0, 2: 1})
-    placement_nine = Placement({0: 2, 1: 2, 2: 2})
-
-    assert not placement_one.valid(dist_med_circ, large_network)
-    assert placement_two.valid(dist_med_circ, large_network)
-    assert placement_three.valid(dist_med_circ, large_network)
-    assert placement_four.valid(dist_med_circ, large_network)
-    assert not placement_five.valid(dist_med_circ, large_network)
-    assert not placement_six.valid(dist_med_circ, large_network)
-    assert not placement_seven.valid(dist_small_circ, small_network)
-    assert placement_eight.valid(dist_small_circ, small_network)
-    assert not placement_nine.valid(dist_small_circ, small_network)
