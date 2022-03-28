6c5c16dc3913a8551238129837dbf09fde703006
Daniel Mills <danielmills@Daniels-MacBook-Pro.local>
2022-03-28 19:46:20 +0100

    Add placement cost test

--- tests/distributor_test.py
+++ tests/distributor_test.py
@@ -250,3 +250,4 @@ def test_q_control_box_circuits():
     ideal_placement = Placement({0: 0, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 1: 1})
 
     assert placement == ideal_placement
+    assert placement.cost(dist_circ, network) == 3
