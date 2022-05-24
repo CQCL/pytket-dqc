31e8d560a868143f7baa4a9ed31ab732482ef26b
Tim Forrer <timforrer16@gmail.com>
2022-05-24 20:56:48 +0900

    add three more commands to export

--- src/pytket_dqc/packing_on_output/__init__.py
+++ src/pytket_dqc/packing_on_output/__init__.py
@@ -3,5 +3,8 @@ from .packing_on_output import (
     build_qubit_op_list,
     get_circuit_qubit_to_server_qubit_map,
     reinsert_cleaned_ops,
-    ordered_list_to_circuit
+    ordered_list_to_circuit,
+    is_antidiagonal,
+    is_diagonal,
+    get_qubit_reg_num,
     )
