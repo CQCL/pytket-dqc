07eb2c01412112a1e3f53ebd7145078cc55e520d
Tim Forrer <timforrer16@gmail.com>
2022-06-02 13:20:09 +0900

    add documentation

--- src/pytket_dqc/packing/packing.py
+++ src/pytket_dqc/packing/packing.py
@@ -20,13 +20,14 @@ def to_bipartite(circuit):
 
     Currently assumes the gateset only has CZ as two qubit gates. The circuit must also have been placed onto servers.
 
+    TODO raise exception if this is not the case?
+
     :param circuit: The circuit for which a bipartite representation is to be found.
     :type circuit: pytket.Circuit
     :return graph: The graph representation of the circuit.
     :rtype: networkx.Graph
     """
-    # Assumes gateset has only CZ as two qubit gates. 
-    indexed_commands = to_indexed_commands(circuit.get_commands()) # Give each command an index to reference it by.
+    extended_commands = to_extended_commands(circuit.get_commands()) # Give each command an index to reference it by.
     next_vertex_index = 0
     graph = Graph()
 
@@ -37,32 +38,32 @@ def to_bipartite(circuit):
         new_vertex = CommandVertex(next_vertex_index, extended_qubits[qubit]) 
         next_vertex_index += 1
 
-    # Populate the IndexedCommands list for each ExtendedQubit
-    for indexed_command in indexed_commands:
-        for qubit in indexed_command.command.qubits:
-            extended_qubits[qubit].add_indexed_command(indexed_command)
+    # Populate the ExtendedCommands list for each ExtendedQubit
+    for extended_command in extended_commands:
+        for qubit in extended_command.command.qubits:
+            extended_qubits[qubit].add_extended_command(extended_command)
 
     # Create all the vertices on each qubit
     for extended_qubit in extended_qubits.values():
-        for indexed_command in extended_qubit.get_indexed_commands():
-            if indexed_command.is_packable() and indexed_command.is_local(): # Local packable gate
+        for extended_command in extended_qubit.get_extended_commands():
+            if extended_command.is_packable() and extended_command.is_local(): # Local packable gate
                 vertex = extended_qubit.last_used_vertex
-                indexed_command.add_vertex(vertex)
-                vertex.add_indexed_command(indexed_command)
+                extended_command.add_vertex(vertex)
+                vertex.add_extended_command(extended_command)
             
-            elif indexed_command.is_1q(): # 1 qubit non-(anti)diagonal gate
+            elif extended_command.is_1q(): # 1 qubit non-(anti)diagonal gate
                 extended_qubit.close_all_vertices()
                 new_vertex = CommandVertex(next_vertex_index, extended_qubit)
-                indexed_command.add_vertex(new_vertex)
-                new_vertex.add_indexed_command(indexed_command)
+                extended_command.add_vertex(new_vertex)
+                new_vertex.add_extended_command(extended_command)
                 extended_qubit.add_vertex(new_vertex)
                 next_vertex_index += 1
 
             else: # Non-local CZ gate
                 currently_connected_servers = extended_qubit.get_currently_connected_servers()
-                if indexed_command.other_arg_server_num(extended_qubit.qubit) in currently_connected_servers.keys():
+                if extended_command.other_arg_server_num(extended_qubit.qubit) in currently_connected_servers.keys():
                     # We are currently connected from this qubit to the server of the other qubit for this CZ, so we can add this CZ to this packing set.
-                    vertex = list(currently_connected_servers[indexed_command.other_arg_server_num(extended_qubit.qubit)])[0]
+                    vertex = list(currently_connected_servers[extended_command.other_arg_server_num(extended_qubit.qubit)])[0]
                     extended_qubit.add_vertex(vertex)
                 
                 else: # Must create a new connection from this qubit to the server.
@@ -72,17 +73,17 @@ def to_bipartite(circuit):
                         extended_qubit.add_vertex(vertex)
                     else:
                         vertex = extended_qubit.last_used_vertex 
-                    other_server_reg_num = indexed_command.other_arg_server_num(extended_qubit.qubit)
+                    other_server_reg_num = extended_command.other_arg_server_num(extended_qubit.qubit)
                     vertex.connect_to_server(other_server_reg_num)
                     
-                indexed_command.add_vertex(vertex)
-                vertex.add_indexed_command(indexed_command)
+                extended_command.add_vertex(vertex)
+                vertex.add_extended_command(extended_command)
 
-                if len(indexed_command.get_vertices()) == 2: # Once both the vertices for the CZ are added, we can add the edge to the graph
-                    other_vertex = [other_vertex for other_vertex in indexed_command.get_vertices() if other_vertex != vertex][0]
+                if len(extended_command.get_vertices()) == 2: # Once both the vertices for the CZ are added, we can add the edge to the graph
+                    other_vertex = [other_vertex for other_vertex in extended_command.get_vertices() if other_vertex != vertex][0]
                     add_edge_to_graph(vertex, other_vertex, graph)
 
-    return graph, indexed_commands
+    return graph, extended_commands
 
 def pack_circuit(bipartite_circuit):
     """Create a circuit from a BipartiteCircuit which has all the StartingProcesses and EndingProcesses included in the circuit.
@@ -95,51 +96,59 @@ def pack_circuit(bipartite_circuit):
     circuit = Circuit()
     le_count = 0
 
-    for indexed_command in bipartite_circuit.indexed_commands:
-        for qubit in indexed_command.command.qubits:
+    for extended_command in bipartite_circuit.extended_commands:
+        for qubit in extended_command.command.qubits:
             if qubit not in circuit.qubits:
                 circuit.add_qubit(qubit)
         
-        add_indexed_command_to_circuit(circuit, indexed_command, bipartite_circuit)
+        add_extended_command_to_circuit(circuit, extended_command, bipartite_circuit)
                 
     return circuit
 
-def add_indexed_command_to_circuit(circuit, indexed_command, bipartite_circuit):
-    """Add an indexed command to a given circuit. If needs be, also adds starting and ending processes to the circuit.
+def add_extended_command_to_circuit(circuit, extended_command, bipartite_circuit):
+    """Add an extended command to a given circuit.
+    
+    If needs be, also adds starting and ending processes to the circuit, as dictated by the supplied BipartiteCircuit.mvc.
+    Note that it is assumed that the global gates are CPhase gates.
 
-    :param circuit: The circuit to which the indexed command is to be appended.
+    TODO Consider the case where we have global CX in the case of qubit swaps e.g. in routing.
+
+    :param circuit: The circuit to which the extended command is to be appended.
     :type circuit: pytket.Circuit
-    :param indexed_command: The indexed command to append.
-    :type indexed_command: IndexedCommand
-    :param bipartite_circuit: The bipartite circuit associated with the indexed commands. 
+    :param extended_command: The extended command to append.
+    :type extended_command: ExtendedCommand
+    :param bipartite_circuit: The bipartite circuit associated with the extended commands. 
     :type bipartite_circuit: BipartiteCircuit
     """
-    # Add an indexed command (IC) to a circuit, with packing if needed as given by the supplied mvc
+    # Add an extended command (EC) to a circuit, with packing if needed as given by the supplied mvc
     # Cases are as follows
-    # 1) The IC is 1q or not packable are no packings on the qubit. -> Just add it to circuit
-    # 2) The IC is not packable but there are packings on the qubit which need to be terminated. -> Terminate all packings, add the IC to circuit
-    # 4) The IC is packable as a 1q diagonal -> Just add it to circuit
-    # 5) The IC is packable as a 1q antidiagonal -> Add to circuit and add correctional X gate to all current packings
-    # 6) The IC is packable as a 2q CZ -> Add the CZ to the packing on the appropriate l_qubit.
+    # 1) The EC is 1q or not packable are no packings on the qubit. -> Just add it to circuit
+    # 2) The EC is not packable but there are packings on the qubit which need to be terminated. -> Terminate all packings, add the EC to circuit
+    # 4) The EC is packable as a 1q diagonal -> Just add it to circuit
+    # 5) The EC is packable as a 1q antidiagonal -> Add to circuit and add correctional X gate to all current packings
+    # 6) The EC is packable as a 2q CZ -> Add the CZ to the packing on the appropriate l_qubit.
 
-    if indexed_command.is_1q() or indexed_command.is_local():
-        vertex = indexed_command.vertices[0]
+    # Handle all the cases where the ExtendedCommand is local.
+    if extended_command.is_local():
+        vertex = extended_command.vertices[0]
         extended_qubit = vertex.extended_qubit
-        if not indexed_command.is_packable():
+        if not extended_command.is_packable(): # If not packable, we need to close off all current packings on this qubit
             while len(extended_qubit.current_l_qubits) > 0:
                 l_qubit = extended_qubit.current_l_qubits[0]
                 l_qubit.stop_packing()
                 circuit.add_custom_gate(end_proc, [], [l_qubit.qubit, l_qubit.origin_extended_qubit.qubit])
             
-        elif extended_qubit.is_packing() and is_antidiagonal(indexed_command.command.op):
+        elif extended_qubit.is_packing() and is_antidiagonal(extended_command.command.op):
             for l_qubit in extended_qubit.current_l_qubits:
                 circuit.X(l_qubit.qubit)
         
-        circuit.add_gate(indexed_command.command.op, indexed_command.command.args)
-        vertex.added_indexed_commands.append(indexed_command)
+        circuit.add_gate(extended_command.command.op, extended_command.command.args)
+        vertex.added_extended_commands.append(extended_command)
 
-    else: #its a global CZ
-        for vertex in indexed_command.vertices: # Add starting processes if needed
+    else: # Handle cases of a global CZ
+        if extended_command.command.op.type == OpType.CX:
+            raise Exception('Global CX gates are not allowed.')
+        for vertex in extended_command.vertices: # Add starting processes if needed
             if vertex.i in bipartite_circuit.mvc and not vertex.is_packing:
                 le_reg_num = vertex.get_connected_server_reg_num()
                 le_reg_name = f'Server {le_reg_num} Link Edge {bipartite_circuit.link_edge_count}'
@@ -150,21 +159,31 @@ def add_indexed_command_to_circuit(circuit, indexed_command, bipartite_circuit):
                 link_qubit.start_packing()
                 circuit.add_custom_gate(start_proc, [], [link_qubit.origin_extended_qubit.qubit, link_qubit.qubit])
 
-        packed_vertex = [vertex for vertex in indexed_command.vertices if vertex.i in bipartite_circuit.mvc][0] #one of the two should be in the mvc...
+        packed_vertex = [vertex for vertex in extended_command.vertices if vertex.i in bipartite_circuit.mvc][0] # If it's a global CZ, one of the two vertices it belongs to must be in the MVC, so select that vertex
         l_qubit = packed_vertex.link_qubit
-        qubit = [qubit for qubit in indexed_command.command.qubits if qubit != l_qubit.origin_extended_qubit.qubit][0]
-        circuit.add_gate(indexed_command.command.op, [qubit, l_qubit.qubit])
-        for vertex in indexed_command.vertices:
-            vertex.added_indexed_commands.append(indexed_command)
+        qubit = [qubit for qubit in extended_command.command.qubits if qubit != l_qubit.origin_extended_qubit.qubit][0]
+        circuit.add_gate(extended_command.command.op, [qubit, l_qubit.qubit])
+        for vertex in extended_command.vertices:
+            vertex.added_extended_commands.append(extended_command)
     
-    for vertex in indexed_command.vertices:
-        if vertex.i in bipartite_circuit.mvc and vertex.indexed_commands == vertex.added_indexed_commands: # If all of the commands on the vertex are packed we add an ending process
+    for vertex in extended_command.vertices:
+        if vertex.i in bipartite_circuit.mvc and vertex.extended_commands == vertex.added_extended_commands: # If all of the commands on the vertex are packed we add an ending process
             l_qubit = vertex.link_qubit
             l_qubit.stop_packing()
             circuit.add_custom_gate(end_proc, [], [l_qubit.qubit, l_qubit.origin_extended_qubit.qubit])
 
 def add_edge_to_graph(vertex1, vertex2, graph):
-    if not(vertex1.is_on_graph or vertex2.is_on_graph):
+    """Given two CommandVertexs and a graph, add an edge to the graph that connects the two vertices.
+    If either of the vertices are not yet on the graph then also add them to the graph.
+
+    :param vertex1: The first vertex we wish to add an edge between.
+    :type vertex1: CommandVertex
+    :param vertex2: The second vertex we wish to add an edge between.
+    :type vertex2: CommandVertex
+    :param graph: The graph to which the edge (and vertices) should be added to.
+    :type graph: networkx.Graph
+    """
+    if not(vertex1.is_on_graph or vertex2.is_on_graph): # Neither vertices are on the graph, so can fix which half of the graph they are on.
         vertex1.set_bipartite_i(0)
         vertex2.set_bipartite_i(1)
         graph.add_nodes_from([vertex1.get_i()], bipartite = vertex1.get_bipartite_i())
@@ -180,11 +199,20 @@ def add_edge_to_graph(vertex1, vertex2, graph):
 
     graph.add_edges_from([(vertex1.get_i(), vertex2.get_i())])
 
-def to_indexed_commands(commands):
-    indexed_commands = []
+def to_extended_commands(commands):
+    """Given a list of commands, convert them to ExtendedCommands.
+    
+    Typically this list of commands comes from pytket.circuit.Circuit.get_commands(). Their index is given by the order in the list they occur at.
+
+    :param commands: A list of commands
+    :type commands: List[pytket.circuit.Command]
+    :return: A list of ExtendedCommands
+    :rtype: List[ExtendedCommand]
+    """
+    extended_commands = []
     for i, command in enumerate(commands):
-        indexed_commands.append(IndexedCommand(i, command))
-    return indexed_commands
+        extended_commands.append(ExtendedCommand(i, command))
+    return extended_commands
 
 class LinkQubit():
     def __init__(self, qubit, vertex):
@@ -208,8 +236,8 @@ class CommandVertex():
         self.i = i
         self.extended_qubit = extended_qubit
         self.reg_num = get_qubit_reg_num(extended_qubit.qubit)
-        self.indexed_commands = []
-        self.added_indexed_commands = []
+        self.extended_commands = []
+        self.added_extended_commands = []
         self.is_open = False
         self.connected_server_reg_num = None
         self.connected_vertices = set()
@@ -233,8 +261,8 @@ class CommandVertex():
     def get_reg_num(self):
         return self.reg_num
 
-    def add_indexed_command(self, indexed_command):
-        self.indexed_commands.append(indexed_command)
+    def add_extended_command(self, extended_command):
+        self.extended_commands.append(extended_command)
 
     def connect_to_server(self, connected_server_reg_num):
         self.connected_server_reg_num = connected_server_reg_num
@@ -245,10 +273,10 @@ class CommandVertex():
         assert vertex.get_reg_num() == self.connected_server_reg_num
         self.is_open = True
 
-    def get_indexed_command_indices(self):
+    def get_extended_command_indices(self):
         indices = []
-        for indexed_command in self.indexed_commands:
-            indices.append(indexed_command.i)
+        for extended_command in self.extended_commands:
+            indices.append(extended_command.i)
         return indices
 
     def set_bipartite_i(self, i): # The bipartite partition on which this vertex resides
@@ -259,9 +287,9 @@ class CommandVertex():
         return self.bipartite_i
 
 class ExtendedQubit():
-    def __init__(self, qubit, indexed_commands):
+    def __init__(self, qubit, extended_commands):
         self.qubit = qubit
-        self.indexed_commands = indexed_commands
+        self.extended_commands = extended_commands
         self.vertices = []
         self.last_used_vertex = None
         self.all_l_qubits = []
@@ -277,11 +305,11 @@ class ExtendedQubit():
     def stop_packing(self, l_qubit):
         self.current_l_qubits.remove(l_qubit)
 
-    def get_indexed_commands(self):
-        return self.indexed_commands
+    def get_extended_commands(self):
+        return self.extended_commands
 
-    def add_indexed_command(self, indexed_command):
-        self.indexed_commands.append(indexed_command)
+    def add_extended_command(self, extended_command):
+        self.extended_commands.append(extended_command)
 
     def add_vertex(self, vertex):
         self.vertices.append(vertex)
@@ -308,8 +336,8 @@ class ExtendedQubit():
 
     def get_command_indices(self):
         command_indices = []
-        for indexed_command in self.get_indexed_commands():
-            command_indices.append(indexed_command.get_i())
+        for extended_command in self.get_extended_commands():
+            command_indices.append(extended_command.get_i())
         return command_indices
 
     def set_last_used_vertex(self, vertex):
@@ -319,7 +347,7 @@ class ExtendedQubit():
         for vertex in self.vertices:
             vertex.close()
 
-class IndexedCommand():
+class ExtendedCommand():
     def __init__(self, i, command):
         self.i = i
         self.command = command
@@ -358,7 +386,7 @@ class IndexedCommand():
 class BipartiteCircuit():
     def __init__(self, circuit):
         self.circuit = circuit
-        self.graph, self.indexed_commands = to_bipartite(circuit)
+        self.graph, self.extended_commands = to_bipartite(circuit)
         self.link_edge_count = 0
         self.top_nodes = {n for n, d in self.graph.nodes(data=True) if d["bipartite"] == 0}
         self.matching = maximum_matching(self.graph, self.top_nodes)
