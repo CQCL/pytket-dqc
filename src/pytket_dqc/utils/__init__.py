9c412976eda098bd4a66a371a4170c84e10715af
Tim Forrer <timforrer16@gmail.com>
2022-08-02 21:05:57 +0900

    add a function to compare circuit equivalency

--- src/pytket_dqc/utils/__init__.py
+++ src/pytket_dqc/utils/__init__.py
@@ -14,4 +14,7 @@ from .op_analysis import (  # noqa:F401
 
 from .graph_tools import direct_from_origin  # noqa:F401
 
-from .circuit_analysis import _cost_from_circuit  # noqa:F401
+from .circuit_analysis import ( # noqa:F401
+    _cost_from_circuit,
+    circuits_are_equivalent
+)
--- src/pytket_dqc/utils/circuit_analysis.py
+++ src/pytket_dqc/utils/circuit_analysis.py
@@ -22,3 +22,33 @@ def _cost_from_circuit(circ: Circuit) -> int:
     assert starting_count == ending_count
 
     return starting_count + telep_count
+
+def circuits_are_equivalent(circuit1, circuit2):
+    """Given two circuits, compare whether the commands,
+    command qubits and quantum registers are equal.
+
+    Essentially compares two circuits and checks if they are equivalent.
+
+    :param circuit1: The first circuit to compare.
+    :type circuit1: pytket.circuit.Circuit
+    :param circuit2: The second circuit to compare.
+    :type circuit2: pytket.circuit.Circuit
+    :return: Whether the two circuits are equivalent.
+    :rtype: bool
+    """
+
+    circuit1_command_names = [command.op.get_name()
+                               for command in circuit1.get_commands()]
+    circuit2_command_names = [
+        command.op.get_name() for command in circuit2.get_commands()]
+
+    circuit1_command_qubits = [
+        command.qubits for command in circuit1.get_commands()]
+    circuit2_command_qubits = [
+        command.qubits for command in circuit2.get_commands()]
+
+    return (
+        circuit1_command_names == circuit2_command_names
+        and circuit1_command_qubits == circuit2_command_qubits
+        and circuit1.q_registers == circuit2.q_registers
+    )
