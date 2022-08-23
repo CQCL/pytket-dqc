4aaf628dd63e4a3c0047c77b49217caec7c5040c
PabloAndresCQ <pablo.andres-martinez@cambridgequantum.com>
2022-08-23 13:31:14 +0100

    Rebase currently working, but Rz, Rx gates are being used instead of Rz, H for practical reasons regarding SquashCustom. There might be a work around this and Tim and I need to think about it to implement the Step 1 from Junyi's Algorithm 29.

--- src/pytket_dqc/utils/gateset.py
+++ src/pytket_dqc/utils/gateset.py
@@ -1,3 +1,4 @@
+import numpy as np
 from pytket.predicates import (  # type: ignore
     GateSetPredicate,
     NoSymbolsPredicate,
@@ -7,17 +8,20 @@ from pytket.predicates import (  # type: ignore
 from pytket import OpType, Circuit
 from pytket.passes import (  # type: ignore
     RebaseCustom,
+    SquashCustom,
+    SequencePass,
 )
 from pytket.circuit import CustomGateDef  # type: ignore
 
 #: Allowed gateset for distributors in pytket-dqc
-dqc_gateset = {
+dqc_1_qubit = {
     OpType.Rz,
-    OpType.X,
-    OpType.H,
-    OpType.CRz,
-    OpType.Measure,
+    OpType.Rx,
 }
+dqc_2_qubit = {
+    OpType.CRz,
+}
+dqc_gateset = {OpType.Measure}.union(dqc_1_qubit).union(dqc_2_qubit)
 
 
 def check_function(circ):
@@ -49,15 +53,18 @@ def tk2_to_crz(a, b, c) -> Circuit:
     return circ
 
 
-def tk1_to_rz_h(a, b, c) -> Circuit:
+def tk1_to_euler(a, b, c) -> Circuit:
     """Given a TK1 gate Rz(a)*Rx(b)*Rz(c), return an equivalent circuit
-    using Rz and H gates.
+    using Rz and Rx gates.
     """
-    return Circuit(1).Rz(c, 0).H(0).Rz(b, 0).H(0).Rz(a, 0)
+    return Circuit(1).Rz(c, 0).Rx(b, 0).Rz(a, 0)
 
 
 #: Pass rebasing gates to those valid within pytket-dqc
-dqc_rebase = RebaseCustom(dqc_gateset, tk2_to_crz, tk1_to_rz_h)
+dqc_rebase = SequencePass([
+    RebaseCustom(dqc_gateset, tk2_to_crz, tk1_to_euler),
+    SquashCustom(dqc_1_qubit, tk1_to_euler),
+])
 
 #: Defining StartingProcess and EndingProcess custom gates
 def_circ = Circuit(2)
