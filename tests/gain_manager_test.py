97070f1079457a2d3c617dd9d666b4f1138c5841
PabloAndresCQ <pablo.andres-martinez@cambridgequantum.com>
2022-09-30 14:56:52 +0100

    Updated the tests in GainManager to use a proper HypergraphCircuit

--- tests/gain_manager_test.py
+++ tests/gain_manager_test.py
@@ -1,159 +1,209 @@
 from pytket_dqc.networks import NISQNetwork
-from pytket_dqc.circuits import Hypergraph, HypergraphCircuit, Distribution
+from pytket_dqc.circuits import (
+    Hyperedge,
+    HypergraphCircuit,
+    Distribution,
+)
 from pytket_dqc.placement import Placement
 from pytket_dqc.allocators import GainManager
-from pytket import Circuit
+from pytket_dqc.utils import steiner_tree
+from pytket import Circuit, OpType
 
-simple_hypergraph = Hypergraph()
-simple_hypergraph.add_vertices(list(range(1, 10)))
-simple_hypergraph.add_hyperedge([1, 3, 4])
-simple_hypergraph.add_hyperedge([1, 2, 4])
-simple_hypergraph.add_hyperedge([3, 4, 8])
-simple_hypergraph.add_hyperedge([4, 5, 7, 8])
-simple_hypergraph.add_hyperedge([3, 7, 9])
-simple_hypergraph.add_hyperedge([5, 6, 7])
-simple_hypergraph.add_hyperedge([2, 5, 6, 9])
+circ = Circuit(4)
+circ.add_gate(OpType.CU1, 0.1234, [1, 2])
+circ.add_gate(OpType.CU1, 0.1234, [0, 2])
+circ.add_gate(OpType.CU1, 0.1234, [2, 3])
+circ.add_gate(OpType.CU1, 0.1234, [0, 3])
+circ.H(0).H(2).Rz(0.1234, 3)
+circ.add_gate(OpType.CU1, 1.0, [0, 2])
+circ.add_gate(OpType.CU1, 1.0, [0, 3])
+circ.add_gate(OpType.CU1, 1.0, [1, 2])
+circ.H(0).H(2).Rz(0.1234, 0)
+circ.add_gate(OpType.CU1, 0.1234, [0, 1])
+circ.add_gate(OpType.CU1, 0.1234, [0, 3])
+circ.add_gate(OpType.CU1, 1.0, [1, 2])
 
-# Creating a dummy instance of HypergraphCircuit to feed to GainManager.
-# This is terrible practice, since we are abusing that we can access
-# the members of ``dummy_circ`` and change them as we please, so that
-# the circuit no longer has anything to do with the hypergraph that we
-# add.
-# The reason for doing this is that GainManager now only accepts a
-# Distribution, which is forced to provide a circuit. Before, GainManager
-# worked on a Hypergraph, and these tests were written with this in mind.
-#
-# Conclusion: this is an ad-hoc way to reuse the existing tests without
-# requiring much extra coding. If further changes are made to GainManager
-# or HypergraphCircuit, it may be best to change these tests.
-dummy_circ = HypergraphCircuit(Circuit(len(simple_hypergraph.vertex_list)))
-dummy_circ.vertex_list = simple_hypergraph.vertex_list
-dummy_circ.hyperedge_list = simple_hypergraph.hyperedge_list
-dummy_circ.hyperedge_dict = simple_hypergraph.hyperedge_dict
-dummy_circ.vertex_neighbours = simple_hypergraph.vertex_neighbours
-for vertex in simple_hypergraph.vertex_list:
-    dummy_circ.vertex_circuit_map[vertex] = {"type": "qubit"}
-
-t_network = NISQNetwork(
-    [[1, 2], [2, 4], [3, 4], [4, 5]],
-    {1: [1, 2, 3, 4], 2: [5], 3: [6, 7, 8], 4: [9], 5: [10, 11, 12]},
-)
-house_network = NISQNetwork(
-    [[1, 2], [2, 4], [1, 3], [3, 4], [3, 5], [4, 5]],
-    {1: [1, 2], 2: [3, 4], 3: [5, 6], 4: [7, 8], 5: [9, 10, 11, 12]},
+network = NISQNetwork(
+    [[0, 1], [0, 2], [0, 3], [3, 4]],
+    {0: [0], 1: [1, 2], 2: [3, 4], 3: [7], 4: [5, 6]},
 )
 
 
-def test_moves():
+def test_placement_move_occupancy():
     placement = Placement(
-        {1: 1, 2: 1, 3: 2, 4: 3, 5: 3, 6: 4, 7: 5, 8: 5, 9: 5}
+        {
+            0: 1,
+            1: 1,
+            2: 2,
+            3: 4,
+            4: 1,
+            5: 1,
+            6: 1,
+            7: 1,
+            8: 2,
+            9: 4,
+            10: 0,
+            11: 1,
+            12: 3,
+            13: 0,
+        }
     )
-    distribution = Distribution(dummy_circ, placement, t_network)
-    t_manager = GainManager(distribution)
+    distribution = Distribution(HypergraphCircuit(circ), placement, network)
+    manager = GainManager(distribution)
 
-    assert t_manager.occupancy[3] == 2
-    assert t_manager.is_move_valid(1, 3)
-    t_manager.move(1, 3)
-    # Server 3 is now full!
-    assert t_manager.occupancy[3] == 3
-    assert not t_manager.is_move_valid(9, 3)
-    # However, since qubit 1 is already in 3, the following is valid
-    assert t_manager.is_move_valid(1, 3)
-    # Server 2 is full
-    assert t_manager.occupancy[2] == 1
-    assert not t_manager.is_move_valid(1, 2)
-    # Let's make some spare space in server 2
-    t_manager.move(3, 1)
-    assert t_manager.occupancy[2] == 0
+    assert manager.occupancy[4] == 1
+    assert manager.is_move_valid(2, 4)
+    manager.move(2, 4)
+    # Server 4 is now full!
+    assert manager.occupancy[4] == 2
+    assert not manager.is_move_valid(1, 4)
+    # However, since qubit 3 is already in 4, the following is valid
+    assert manager.is_move_valid(3, 4)
+    # Moreover, gate vertices can always be moved around
+    assert manager.is_move_valid(8, 4)
+    # Restore original placement
+    manager.move(2, 2)
+
+    # Server 1 is full
+    assert manager.occupancy[1] == len(network.server_qubits[1])
+    assert not manager.is_move_valid(3, 1)
+    # Let's make some spare space in server 1
+    manager.move(0, 0)
+    assert manager.occupancy[1] < len(network.server_qubits[1])
     # Now the move should be valid
-    assert t_manager.is_move_valid(1, 2)
-    t_manager.move(1, 2)
-    assert t_manager.occupancy[2] == 1
-    assert t_manager.occupancy[3] == 2
+    assert manager.is_move_valid(3, 1)
+    manager.move(3, 1)
+    assert manager.occupancy[0] == 1
+    assert manager.occupancy[1] == 2
+    assert manager.occupancy[4] == 0
 
 
-def test_gain_on_t_network():
+def test_gain_no_embeddings():
     placement = Placement(
-        {1: 1, 2: 1, 3: 2, 4: 3, 5: 3, 6: 4, 7: 5, 8: 5, 9: 5}
+        {
+            0: 1,
+            1: 1,
+            2: 2,
+            3: 4,
+            4: 1,
+            5: 1,
+            6: 1,
+            7: 1,
+            8: 2,
+            9: 4,
+            10: 0,
+            11: 1,
+            12: 3,
+            13: 0,
+        }
     )
-    distribution = Distribution(dummy_circ, placement, t_network)
-    t_manager = GainManager(distribution)
+    distribution = Distribution(HypergraphCircuit(circ), placement, network)
+    manager = GainManager(distribution)
 
     # Moving a vertex to where it already is has no gain
-    assert t_manager.gain(1, 1) == 0
+    assert manager.gain(0, 1) == 0
 
-    steiner_cost_1235 = t_manager.steiner_cost(frozenset([1, 2, 3, 5]))
-    steiner_cost_123 = t_manager.steiner_cost(frozenset([1, 2, 3]))
-    steiner_cost_135 = t_manager.steiner_cost(frozenset([1, 3, 5]))
-    steiner_cost_235 = t_manager.steiner_cost(frozenset([2, 3, 5]))
-    steiner_cost_13 = t_manager.steiner_cost(frozenset([1, 3]))
-    steiner_cost_35 = t_manager.steiner_cost(frozenset([3, 5]))
-    assert steiner_cost_1235 == 4
+    g = manager.server_graph
+
+    steiner_cost_13 = len(steiner_tree(g, [1, 3]).edges)
+    steiner_cost_34 = len(steiner_tree(g, [3, 4]).edges)
+    steiner_cost_14 = len(steiner_tree(g, [1, 4]).edges)
+    steiner_cost_4 = len(steiner_tree(g, [4]).edges)
+    steiner_cost_1 = len(steiner_tree(g, [1]).edges)
+    steiner_cost_124 = len(steiner_tree(g, [1, 2, 4]).edges)
+    steiner_cost_12 = len(steiner_tree(g, [1, 2]).edges)
+    steiner_cost_123 = len(steiner_tree(g, [1, 2, 3]).edges)
+    steiner_cost_24 = len(steiner_tree(g, [2, 4]).edges)
+    assert steiner_cost_13 == 2
+    assert steiner_cost_34 == 1
+    assert steiner_cost_14 == 3
+    assert steiner_cost_4 == 0
+    assert steiner_cost_1 == 0
+    assert steiner_cost_124 == 4
+    assert steiner_cost_12 == 2
     assert steiner_cost_123 == 3
-    assert steiner_cost_135 == 4
-    assert steiner_cost_235 == 3
-    assert steiner_cost_13 == 3
-    assert steiner_cost_35 == 2
+    assert steiner_cost_24 == 3
 
-    # Make some space in server 5
-    assert t_manager.is_move_valid(8, 1)
-    t_manager.move(8, 1)
-    assert t_manager.is_move_valid(9, 1)
-    t_manager.move(9, 1)
-
-    # The two hyperedges incident to vertex 1 connect servers [1,2,3] & [1,3]
+    # The two hyperedges incident to vertex 12 are [0,11,12] and [3,9,12]
+    # which are allocated to servers as follows:   [1, 1, 3] and [4,4, 3].
     # If we do the following move:
-    assert t_manager.is_move_valid(1, 5)
-    # then the two hyperedges would connect servers [2,3,5] and [1,3,5]
-    # the gains should look like this:
-    current_cost = steiner_cost_123 + steiner_cost_13
-    new_cost = steiner_cost_235 + steiner_cost_135
-    assert t_manager.gain(1, 5) == current_cost - new_cost
-    # Instead, let's move vertex 3 to server 5 so that server 2 is empty
-    assert t_manager.is_move_valid(3, 5)
-    t_manager.move(3, 5)
-    assert t_manager.occupancy[2] == 0
-    # Now that the two hyperedges connect servers [1,3,5] and [1,3]
-    # If we do the following move:
-    assert t_manager.is_move_valid(1, 5)
-    # then the two hyperedges would connect servers [3,5] and [1,3,5]
+    assert manager.is_move_valid(12, 4)
+    # then the allocation is as follows:           [1, 1, 4] and [4,4, 4].
     # The gains should look like this:
-    current_cost = steiner_cost_135 + steiner_cost_13
-    new_cost = steiner_cost_35 + steiner_cost_135
-    assert t_manager.gain(1, 5) == current_cost - new_cost
-
-
-def test_gain_and_steiner_cache_on_house_network():
-    placement = Placement(
-        {1: 1, 2: 1, 3: 2, 4: 3, 5: 3, 6: 4, 7: 5, 8: 5, 9: 5}
+    current_cost = steiner_cost_13 + steiner_cost_34
+    new_cost = steiner_cost_14 + steiner_cost_4
+    assert manager.gain(12, 4) == current_cost - new_cost
+    # Check that, indeed, the costs are updated accordingly
+    assert (
+        manager.hyperedge_cost_map[Hyperedge([0, 11, 12])] == steiner_cost_13
     )
-    distribution = Distribution(
-        dummy_circ, placement, house_network
+    assert manager.hyperedge_cost_map[Hyperedge([3, 9, 12])] == steiner_cost_34
+    manager.move(12, 4)
+    assert (
+        manager.hyperedge_cost_map[Hyperedge([0, 11, 12])] == steiner_cost_14
     )
-    house_manager = GainManager(distribution)
+    assert manager.hyperedge_cost_map[Hyperedge([3, 9, 12])] == steiner_cost_4
+    # Move back
+    manager.move(12, 3)
 
-    house_manager.steiner_cost(frozenset([2, 5]))
-    house_manager.steiner_cost(frozenset([3, 4, 5]))
-    house_manager.steiner_cost(frozenset([3, 5]))
-    house_manager.steiner_cost(frozenset([2, 3, 5]))
-    house_manager.steiner_cost(frozenset([2, 5]))
-    house_manager.steiner_cost(frozenset([2, 3, 4]))
-
-    # The three hyperedges incident to vertex 7 connect servers:
-    #   [3,5] and [2,5] and [3,4,5]
+    # The hyperedges incident to vertex 0 are [0,5,7], [0,11,12] and [0,8,9]
+    # which are allocated to servers:         [1,1,1], [1, 1, 3] and [1,2,4].
     # If we do the following move:
-    assert house_manager.is_move_valid(7, 2)
-    # then the three hyperedges would connect servers:
-    #   [2,3,5] and [2,5] and [2,3,4]
+    assert manager.is_move_valid(0, 2)
+    # then the allocation is as folows:       [2,1,1], [2, 1, 3] and [2,2,4].
     # The gains should look like this:
-    current_cost = (
-        house_manager.steiner_cache[frozenset([3, 5])]
-        + house_manager.steiner_cache[frozenset([2, 5])]
-        + house_manager.steiner_cache[frozenset([3, 4, 5])]
+    current_cost = steiner_cost_1 + steiner_cost_13 + steiner_cost_124
+    new_cost = steiner_cost_12 + steiner_cost_123 + steiner_cost_24
+    assert manager.gain(0, 2) == current_cost - new_cost
+    # Check that, indeed, the costs are updated accordingly
+    assert manager.hyperedge_cost_map[Hyperedge([0, 5, 7])] == steiner_cost_1
+    assert (
+        manager.hyperedge_cost_map[Hyperedge([0, 11, 12])] == steiner_cost_13
     )
-    new_cost = (
-        house_manager.steiner_cache[frozenset([2, 3, 5])]
-        + house_manager.steiner_cache[frozenset([2, 5])]
-        + house_manager.steiner_cache[frozenset([2, 3, 4])]
+    assert manager.hyperedge_cost_map[Hyperedge([0, 8, 9])] == steiner_cost_124
+    manager.move(0, 2)
+    assert manager.hyperedge_cost_map[Hyperedge([0, 5, 7])] == steiner_cost_12
+    assert (
+        manager.hyperedge_cost_map[Hyperedge([0, 11, 12])] == steiner_cost_123
     )
-    assert house_manager.gain(7, 2) == current_cost - new_cost
+    assert manager.hyperedge_cost_map[Hyperedge([0, 8, 9])] == steiner_cost_24
+    # Move back
+    manager.move(0, 1)
+
+    # Check that the cache is correct
+    assert (
+        manager.steiner_cache[frozenset([1, 3])].edges
+        == steiner_tree(g, [1, 3]).edges
+    )
+    assert (
+        manager.steiner_cache[frozenset([3, 4])].edges
+        == steiner_tree(g, [3, 4]).edges
+    )
+    assert (
+        manager.steiner_cache[frozenset([1, 4])].edges
+        == steiner_tree(g, [1, 4]).edges
+    )
+    assert (
+        manager.steiner_cache[frozenset([4])].edges
+        == steiner_tree(g, [4]).edges
+    )
+    assert (
+        manager.steiner_cache[frozenset([1])].edges
+        == steiner_tree(g, [1]).edges
+    )
+    assert (
+        manager.steiner_cache[frozenset([1, 2, 4])].edges
+        == steiner_tree(g, [1, 2, 4]).edges
+    )
+    assert (
+        manager.steiner_cache[frozenset([1, 2])].edges
+        == steiner_tree(g, [1, 2]).edges
+    )
+    assert (
+        manager.steiner_cache[frozenset([1, 2, 3])].edges
+        == steiner_tree(g, [1, 2, 3]).edges
+    )
+    assert (
+        manager.steiner_cache[frozenset([2, 4])].edges
+        == steiner_tree(g, [2, 4]).edges
+    )
