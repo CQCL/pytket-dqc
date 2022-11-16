919e5ddf2fef5820d017607c04080ce19cc3881e
Daniel Mills <danielmills@Daniels-MacBook-Pro.local>
2022-11-16 11:26:54 +0000

    Add test showing limitations

--- tests/refiner_test.py
+++ tests/refiner_test.py
@@ -8,6 +8,98 @@ from pytket_dqc.circuits.hypergraph import Hyperedge
 from pytket_dqc.refiners import SequentialDTypeMerge
 
 
+def test_sequential_merge_d_type_backwards_meregable():
+    # Note that this test identifies the limits of SequentialDTypeMerge.
+    # In particular there are hyperedges which could be merged
+    # but are missed by this greedy approach.
+
+    ideal_hyperedge_list = [
+        Hyperedge(vertices=[0, 4, 7, 8, 11], weight=1),
+        Hyperedge(vertices=[1, 4, 7], weight=1),
+        Hyperedge(vertices=[1, 6, 10], weight=1),
+        Hyperedge(vertices=[1, 8, 11], weight=1),
+        Hyperedge(vertices=[1, 9], weight=1),
+        Hyperedge(vertices=[2, 9], weight=1),
+        Hyperedge(vertices=[3, 5, 6, 10], weight=1),
+    ]
+
+    test_network = NISQNetwork(
+        server_coupling=[[0, 1], [1, 2], [1, 3]],
+        server_qubits={0: [0], 1: [1], 2: [2], 3: [3]}
+    )
+
+    test_circuit = Circuit(4)
+
+    test_circuit.add_gate(OpType.CU1, 1.0, [0, 1])
+    test_circuit.H(1)
+    test_circuit.add_gate(OpType.CU1, 1.0, [1, 3])
+    test_circuit.H(1)
+    test_circuit.add_gate(OpType.CU1, 1.0, [1, 3])
+    test_circuit.add_gate(OpType.CU1, 1.0, [0, 1])
+    test_circuit.H(1)
+    test_circuit.add_gate(OpType.CU1, 1.0, [0, 1])
+    test_circuit.H(1)
+    test_circuit.add_gate(OpType.CU1, 1.0, [1, 2])
+    test_circuit.add_gate(OpType.CU1, 1.0, [1, 3])
+    test_circuit.H(1)
+    test_circuit.add_gate(OpType.CU1, 1.0, [0, 1])
+
+    test_hyp_circuit = HypergraphCircuit(test_circuit)
+
+    test_hyp_circuit.vertex_neighbours = {
+        i: set() for i in test_hyp_circuit.vertex_list
+    }
+    test_hyp_circuit.hyperedge_list = []
+    test_hyp_circuit.hyperedge_dict = {
+        i: [] for i in test_hyp_circuit.vertex_list
+    }
+
+    new_hyperedge_list = [
+        [0, 4, 7, 8, 11],
+        [1, 4, 7],
+        [1, 6, 10],
+        [1, 8, 11],
+        [1, 9],
+        [2, 9],
+        [3, 5, 6, 10],
+    ]
+
+    for new_hyperedge in new_hyperedge_list:
+        test_hyp_circuit.add_hyperedge(new_hyperedge)
+
+    test_placement = Placement(
+        {
+            0: 0,
+            1: 1,
+            2: 2,
+            3: 3,
+            4: 0,
+            5: 3,
+            6: 3,
+            7: 0,
+            8: 0,
+            9: 2,
+            10: 3,
+            11: 0
+        }
+    )
+
+    distribution = Distribution(
+        circuit=test_hyp_circuit,
+        placement=test_placement,
+        network=test_network,
+    )
+
+    assert distribution.cost() == 7
+    assert distribution.circuit.hyperedge_list == ideal_hyperedge_list
+
+    refiner = SequentialDTypeMerge()
+    refiner.refine(distribution)
+
+    assert distribution.cost() == 7
+    assert distribution.circuit.hyperedge_list == ideal_hyperedge_list
+
+
 def test_sequential_merge_d_type_intertwined():
 
     test_network = NISQNetwork(
