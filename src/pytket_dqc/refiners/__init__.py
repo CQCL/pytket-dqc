f3790c4c1f8c07d3f96d0ee6e045f2e98bf47e42
Tim Forrer <timforrer16@gmail.com>
2022-12-24 16:39:03 +0900

    update __init__.py to import changed class name

--- src/pytket_dqc/refiners/__init__.py
+++ src/pytket_dqc/refiners/__init__.py
@@ -1,6 +1,6 @@
 from .refiner import Refiner  # noqa:F401
 from .neighbouring_d_type_merge import NeighbouringDTypeMerge  # noqa:F401
 from .intertwined_d_type_merge import IntertwinedDTypeMerge  # noqa:F401
-from .n_h_type_merge import NHTypeGreedyMerge  # noqa:F401
+from .n_h_type_merge import EagerHTypeMerge  # noqa:F401
 from .repeat_refiner import RepeatRefiner  # noqa:F401
 from .sequence_refiner import SequenceRefiner  # noqa:F401
