43c2a20311116459d7709d5c48bfaf9a74f8e520
PabloAndresCQ <pablo.andres-martinez@cambridgequantum.com>
2023-01-11 10:56:51 +0000

    Removed xfail annotation from a couple of tests

--- tests/refiner_test.py
+++ tests/refiner_test.py
@@ -1,4 +1,3 @@
-import pytest
 import json  # type: ignore
 from pytket import Circuit, OpType  # type: ignore
 from pytket_dqc import Distribution
@@ -780,7 +779,6 @@ def test_vertex_cover_refiner_complex_2():
     )
 
 
-@pytest.mark.xfail(reason="Currently failing. Branch #66 fixes it.")
 def test_vertex_cover_refiner_pauli_circ():
     # Randomly generated circuit of type pauli, depth 10 and 10 qubits
     with open(
