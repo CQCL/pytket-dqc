9f2b0166995268f31223df9837df2142e69f6966
Dan Mills <52407433+daniel-mills-cqc@users.noreply.github.com>
2023-04-19 09:23:40 +0100

    Add copyright information (#95)
    * Add copyright information
    
    * Add university of tokyo
    
    * Remove flag
    
    * Add copyright to distributor init
    
    ---------
    
    Co-authored-by: dan-mills-cqc <danielmills@daniels-mbp.home>
    Co-authored-by: dan-mills-cqc <danielmills@Daniels-MacBook-Pro.local>

--- src/pytket_dqc/networks/nisq_network.py
+++ src/pytket_dqc/networks/nisq_network.py
@@ -1,3 +1,17 @@
+# Copyright 2023 Quantinuum and The University of Tokyo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from __future__ import annotations
 from pytket_dqc.networks.server_network import ServerNetwork
 from itertools import combinations
@@ -108,8 +122,7 @@ class NISQNetwork(ServerNetwork):
         :return: Dictionary serialisation of NISQNetwork. Dictionary has keys
             'server_coupling' and 'server_qubits'.
         :rtype: dict[str, Union[
-                list[list[int]], dict[int, list[int]], dict[int, int]
-            ]]
+            list[list[int]], dict[int, list[int]], dict[int, int]]]
         """
 
         return {
